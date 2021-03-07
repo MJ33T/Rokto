8a5a2562d88b47c8270386400001069e29c85ac6
J33T <muhtasim_jeet@hotmail.com>
2021-03-07 23:42:26 +0600

    creating path for navbar tabs

--- Rokto_App/urls.py
+++ Rokto_App/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('find_donar/', views.find_donar, name='find_donar'),
+    path('new_donar/', views.new_donar, name='new_donar'),
 ]
--- Rokto_App/views.py
+++ Rokto_App/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def home(request):
     return render(request, 'Rokto_App/index.html')
+
+
+def find_donar(request):
+    return render(request, 'Rokto_App/find_donar.html')
+
+
+def new_donar(request):
+    return render(request, 'Rokto_App/new_donar.html')
